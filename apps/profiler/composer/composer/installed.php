4bfae8061af445cda1726b3bc8381a6874a1805c
Carl Schwan <carl@carlschwan.eu>
2022-01-19 23:30:34 +0100

    Start implementing a built-in profiler inside Nextcloud
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

---
+++ apps/profiler/composer/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../',
+        'aliases' => array(),
+        'reference' => '7619dceff4bf30b34cd048bc2b93797ee9c8b759',
+        'name' => '__root__',
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../',
+            'aliases' => array(),
+            'reference' => '7619dceff4bf30b34cd048bc2b93797ee9c8b759',
+            'dev_requirement' => false,
+        ),
+    ),
+);
